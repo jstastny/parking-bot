c4a29ae2fd15f4b7c7ca7efb70c8815d9cad0b4c
Jan Stastny <honza@stastnarodina.com>
2017-08-29 11:11:07 +0200

    Order parking spots desc by priority

--- app/models/parking_spot.rb
+++ app/models/parking_spot.rb
@@ -2,7 +2,7 @@ class ParkingSpot < ApplicationRecord
   has_many :reservations
 
   scope :available_spots, ->(from, to) {
-    where('NOT EXISTS (SELECT * FROM reservations WHERE "from" <= ? and "to" >= ? and parking_spot_id = parking_spots.id)', to, from)
+    where('NOT EXISTS (SELECT * FROM reservations WHERE "from" <= ? and "to" >= ? and parking_spot_id = parking_spots.id)', to, from).order(priority: :desc)
   }
 
 end
