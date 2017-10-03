4a18bfccc75ca3e79b5ff6dd9c1d436c44be8d59
Jan Stastny <honza@stastnarodina.com>
2017-10-03 13:40:01 +0200

    Bullet instead of dash

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -15,7 +15,7 @@ class Bot < SlackRubyBot::Bot
       unless reservations.empty?
         message += "There are following reservations:\n"
         reservations.each do |reservation|
-          message += " - Parking spot #{reservation.parking_spot.name} reserved by #{reservation.user.display_name}\n"
+          message += " • Parking spot #{reservation.parking_spot.name} reserved by #{reservation.user.display_name}\n"
         end
       end
 
